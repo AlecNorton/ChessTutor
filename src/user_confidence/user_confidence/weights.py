d1b9b24b54f35fc5b111fc8c00477d27040b3120
khyat8 <khyatsharma8@gmail.com>
2026-05-05 14:34:25 -0400

    confidence format change and tts flag

--- src/user_confidence/user_confidence/weights.py
+++ src/user_confidence/user_confidence/weights.py
@@ -3,6 +3,7 @@ A simple Python dict mapping node names to float weights."""
 
 DEFAULT_WEIGHTS = {
     'face': 1.0,  # weight for facial expression recognition
+    'llm': 1.0,   # weight for LLM-perceived confidence from tutor_node
     # 'gaze': 0.0, # probably wont do this
     # 'voice': 0.0, # TODO: tune based on accuracy
     # 'game': 0.0 # TODO: tune for game/position difficulty
